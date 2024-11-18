93c618d119a9ee54e39e00f2aee7b9846faf3ae7
nikitaivanov <iwaa0303@mail.ru>
2024-11-18 16:48:20 +0300

    added ci

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ RUN chmod +x /app/app.jar
 EXPOSE 8761
 
 # Запуск приложения
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["java", "-jar", "app.jar", "--spring.profiles.active=prod"]
--- src/test/kotlin/org/example/noteeurekaserviceregistry/NoteEurekaServiceRegistryApplicationTests.kt
+++ src/test/kotlin/org/example/noteeurekaserviceregistry/NoteEurekaServiceRegistryApplicationTests.kt
@@ -2,8 +2,10 @@ package org.example.noteeurekaserviceregistry
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class NoteEurekaServiceRegistryApplicationTests {
 
     @Test
